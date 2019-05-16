e3f17e21585cc927424763a89f2556bda8dd1cc9
aharabara <alexandr.harabara@soluti.fr>
2019-05-16 21:04:29 +0300

    - refactoring

--- 5/index.php
+++ 5/index.php
@@ -1,17 +1,10 @@
 <?php
 
-use Base\{Application, Button, Divider, Input, Label, OrderedList, View, TextArea, Panel};
+use Base\{Application, View};
 
 chdir(__DIR__);
 require './vendor/autoload.php';
 
-View::registerComponent('button', Button::class);
-View::registerComponent('panel', Panel::class);
-View::registerComponent('div', Divider::class);
-View::registerComponent('input', Input::class);
-View::registerComponent('label', Label::class);
-View::registerComponent('textarea', TextArea::class);
-View::registerComponent('list', OrderedList::class);
 $mainView = (new View())
     ->parse('./views/surfaces.xml')
     ->parse('./views/main.xml');
